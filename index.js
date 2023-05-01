b3b533edb8198b8e3d00662c50c8f1d25570fd3e
Rajan-007 <aravinthrajan034@gmail.com>
2023-05-01 22:28:48 +0530

    arrays(2d Array)

--- index.js
+++ index.js
@@ -22,3 +22,21 @@ console.log(points.length);
 console.log(points[2]);
 
 console.log(cities[cities.length-1]);
+
+// mix of int & string 
+
+let arr = [5,6,7,"abcd","efgh",[1,2,3]];
+console.log(arr[5][0]);
+
+// 2D Array
+
+let matrix = [[3,4,0],[5,6,9],[7,6,8]];
+console.log(matrix[0][2]);
+
+console.log(matrix[2]);
+
+// array methods
+
+matrix[3].push("hello");
+console.log(matrix);
+console.log(matrix.length);
