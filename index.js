9301c8940b2b17afdeb89370d2ac32f7d9db06b3
Rajan-007 <aravinthrajan034@gmail.com>
2023-05-01 22:12:59 +0530

    arrays(length & index)

--- index.js
+++ index.js
@@ -6,3 +6,19 @@ let place = " chennai";
 
 let msg = `${firstname} ${lastname} lives in ${place}`;
 console.log(msg);
+
+// Arrays
+
+let cities = ["chennai ","mumbai","kolkata","delhi"];
+let points = [85,58,88,90,98];
+
+// length-total elements of array
+
+console.log(cities.length);
+console.log(points.length);
+
+// Indexing
+
+console.log(points[2]);
+
+console.log(cities[cities.length-1]);
